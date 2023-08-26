0b31ac6cca9d7aa834fb2d70c92b881bedd6ce92
Rene Matorel <renealejandrojr@hotmail.com>
2023-08-26 16:57:47 -0500

    comit inicial header

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Headermtr from './conponents/Headermtr';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Pricing</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" aria-disabled="true">Desactivado</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Headermtr></Headermtr>
       </header>
     </div>
   );
